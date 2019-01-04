d992f4722cb71502775cf19516102a01b55c2be8
franz1450 <franz@database.it>
2019-01-04 01:49:09 +0100

    Commit test2

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!!!");jhg
+            Console.WriteLine("Hello world!!!");
         }
     }
 }
